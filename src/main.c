3488cc0cd79f7f7cf0bf7fe4b6d7447587e3a137
IFo Hancroft <contact@ifohancroft.com>
2021-10-05 04:09:55 +0300

    Testing my fix

--- src/main.c
+++ src/main.c
@@ -77,9 +77,6 @@ int main(int argc, char** argv)
 
     optionsParse(argc, argv);
 
-    initXAndImlib(opt.display, 0);
-    atexit(uninitXAndImlib);
-
     if (!opt.outputFile) {
         opt.outputFile = strdup("%Y-%m-%d-%H%M%S_$wx$h_scrot.png");
         opt.thumbFile = strdup("%Y-%m-%d-%H%M%S_$wx$h_scrot-thumb.png");
@@ -87,6 +84,9 @@ int main(int argc, char** argv)
         opt.thumbFile = nameThumbnail(opt.outputFile);
         scrotHaveFileExtension(opt.outputFile, &haveExtension);
     }
+
+    initXAndImlib(opt.display, 0);
+    atexit(uninitXAndImlib);
 
     if (opt.focused)
         image = scrotGrabFocused();
